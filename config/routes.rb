18c7a5b7fac3cadb3e62a4e6ae871258ed076bb4
tienmc <123456+tienmc@users.noreply.github.com>
2023-12-26 09:51:01 +0700

    Upload Cover Images in Rails with ActiveStorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :blog_posts
+
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
+
 
   root 'blog_posts#index'
 end
